20dd6151dfc329aa23607dcafd752c10efd191ae
root <root@DESKTOP-HAE1343.localdomain>
2024-11-17 13:00:42 -0300

    W03

--- src/Wordle.hs
+++ src/Wordle.hs
@@ -3,31 +3,50 @@ module Wordle where
 import Core
 import Data.Char (isUpper)
 
-data Juego = Juego {objetivo :: String, cantIntentos :: Int, intentos :: Intentos}
+data Juego = Juego {objetivo :: String, intentosTotales :: Int, intentosDisp :: Int, intentos :: Intentos}
   deriving (Show, Eq)
 
-data Estado = Terminado | EnProgresoNV | EnProgresoV
+type Intentos = [[(Char, Match)]]
+
+data Estado = EnProgresoNV | EnProgresoV | Adivino | NoAdivino
   deriving (Show, Eq)
 
 data ResultadoValidacion = OK | LongitudInvalida | CharInvalido
   deriving (Show, Eq)
 
-type Intentos = [[(Char, Match)]]
-
-inicializarIntentos :: Juego -> Intentos
-inicializarIntentos j = replicate (cantIntentos j) []
+crearJuego :: String -> Int -> Juego
+crearJuego objetivo intentosTotales =
+  Juego
+    { objetivo = objetivo,
+      intentosTotales = intentosTotales,
+      intentosDisp = intentosTotales,
+      intentos = replicate intentosTotales []
+    }
 
 tieneTilde :: String -> Bool
 tieneTilde intento = any (`elem` "áéíóúÁÉÍÓÚ") intento
 
 validarInput :: String -> Juego -> ResultadoValidacion
 validarInput input j
-  | length (input) /= length (objetivo j) = LongitudInvalida
+  | length (input) /= obtenerLongitudObjetivo j = LongitudInvalida
   | tieneTilde input || not (all isUpper input) = CharInvalido
   | otherwise = OK
 
 enviarIntento :: String -> Juego -> (Estado, Juego)
 enviarIntento intento j
-  | (cantIntentos j) == 0 = (Terminado, j)
+  | (obtenerIntentosDisp j) == 0 = (NoAdivino, j)
   | (validarInput intento j) /= OK = (EnProgresoNV, j)
-  | otherwise = (EnProgresoV, j {cantIntentos = (cantIntentos j) - 1, intentos = (intentos j) ++ [match (objetivo j) intento]})
+  | intento == objetivo j = (Adivino, j {intentosDisp = (obtenerIntentosDisp j) - 1, intentos = actualizarPosicion j intento})
+  | otherwise = (EnProgresoV, j {intentosDisp = (obtenerIntentosDisp j) - 1, intentos = actualizarPosicion j intento})
+
+actualizarPosicion :: Juego -> String -> Intentos
+actualizarPosicion j intento = [if i == (obtenerIntentosTotales j - obtenerIntentosDisp j) then match (objetivo j) intento else (intentos j) !! i | i <- [0 .. obtenerIntentosTotales j - 1]]
+
+obtenerLongitudObjetivo :: Juego -> Int
+obtenerLongitudObjetivo j = length (objetivo j)
+
+obtenerIntentosDisp :: Juego -> Int
+obtenerIntentosDisp j = intentosDisp j
+
+obtenerIntentosTotales :: Juego -> Int
+obtenerIntentosTotales j = intentosTotales j
